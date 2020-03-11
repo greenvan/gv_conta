1b5d9c5a58ba7fb922553a92d1707dd14823d4a9
GreenVan <greenvanwebmaster@gmail.com>
2020-03-11 10:26:05 +0100

    Added dynamic_treeview

--- lib/model/category.dart
+++ lib/model/category.dart
@@ -1,9 +1,32 @@
-class Category {
-  String name;
-  String type;
-  String id;
-  String parent_id;
-  String full_path;
+import 'package:gvconta/widgets/dynamic_treeview.dart';
 
-  Category(this.name, this.type, this.id, this.parent_id, this.full_path);
+class Category implements BaseData {
+  final int id;
+  final int parentId;
+  String name;
+  //String fullpath;
+
+  ///Any extra data you want to get when tapped on children
+  Map<String, dynamic> extras;
+
+  Category({this.id, this.parentId, this.name, this.extras});
+  @override
+  String getId() {
+    return this.id.toString();
+  }
+
+  @override
+  Map<String, dynamic> getExtraData() {
+    return this.extras;
+  }
+
+  @override
+  String getParentId() {
+    return this.parentId.toString();
+  }
+
+  @override
+  String getTitle() {
+    return this.name;
+  }
 }
--- lib/pages/category_list_page.dart
+++ lib/pages/category_list_page.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:gvconta/model/account.dart';
+import 'package:gvconta/model/category.dart';
 import 'package:gvconta/model/user.dart';
 import 'package:gvconta/widgets/navigation_arguments.dart';
+import 'package:gvconta/widgets/dynamic_treeview.dart';
 
 class CategoryListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    NavigationArguments args = ModalRoute.of(context).settings.arguments;
-    final User user = args.user;
+    final User user = ModalRoute.of(context).settings.arguments;
 
     //TODO ver si necesitamos un evento o no
 
@@ -24,9 +24,160 @@ class CategoryListPage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Center(
-          child: Text(
-              'Esta es la página en la que se podrá ver la lista de categorías  de gastos e ingresos asociados al usuario ' +
-                  user.toString()),
+          child: DynamicTreeView(
+            data: getData(),
+            config: Config(
+                parentTextStyle:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.w600),
+                rootId: "1",
+                parentPaddingEdgeInsets:
+                    EdgeInsets.only(left: 16, top: 0, bottom: 0)),
+            onTap: (m) {
+              print("onChildTap -> $m");
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (ctx) => ScreenTwo(
+                            data: m,
+                          )));
+            },
+            width: MediaQuery.of(context).size.width,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//TODO: sacar los datos de la base de datos
+
+List<BaseData> getData() {
+  return [
+    Category(
+      id: 1,
+      name: 'Root',
+      parentId: -1,
+      extras: {'key': 'extradata1'},
+    ),
+    Category(
+      id: 2,
+      name: 'Gastos',
+      parentId: 1,
+      extras: {'key': 'extradata2'},
+    ),
+    Category(
+      id: 3,
+      name: 'Transporte',
+      parentId: 2,
+      extras: {'key': 'extradata3'},
+    ),
+    Category(
+      id: 4,
+      name: 'Comida',
+      parentId: 2,
+      extras: {'key': 'extradata4'},
+    ),
+    Category(
+      id: 5,
+      name: 'Ingresos',
+      parentId: 1,
+      extras: {'key': 'extradata5'},
+    ),
+    Category(
+      id: 6,
+      name: 'Sueldo',
+      parentId: 5,
+      extras: {'key': 'extradata6'},
+    ),
+    Category(
+      id: 7,
+      name: 'Loteria',
+      parentId: 5,
+      extras: {'key': 'extradata7'},
+    ),
+    Category(
+      id: 8,
+      name: 'Combustible',
+      parentId: 3,
+      extras: {'key': 'extradata8'},
+    ),
+    Category(
+      id: 9,
+      name: 'Creditrans',
+      parentId: 3,
+      extras: {'key': 'extradata9'},
+    ),
+    Category(
+      id: 10,
+      name: 'Autopista',
+      parentId: 3,
+      extras: {'key': 'extradata10'},
+    ),
+    Category(
+      id: 11,
+      name: 'Comidas Fuera',
+      parentId: 4,
+      extras: {'key': 'extradata11'},
+    ),
+    Category(
+      id: 12,
+      name: 'Supermercado',
+      parentId: 4,
+      extras: {'key': 'extradata12'},
+    ),
+    Category(
+      id: 13,
+      name: 'UPV',
+      parentId: 6,
+      extras: {'key': 'extradata13'},
+    ),
+    Category(
+      id: 14,
+      name: 'clases',
+      parentId: 6,
+      extras: {'key': 'extradata14'},
+    ),
+    Category(
+      id: 15,
+      name: 'Miravalles',
+      parentId: 14,
+      extras: {'key': 'extradata15'},
+    ),
+    Category(
+      id: 16,
+      name: 'Izaskun',
+      parentId: 14,
+      extras: {'key': 'extradata16'},
+    ),
+  ];
+}
+
+class ScreenTwo extends StatelessWidget {
+  final Map data;
+  ScreenTwo({this.data});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("${data['title']}"),
+      ),
+      body: Center(
+        child: Wrap(
+          direction: Axis.vertical,
+          children: <Widget>[
+            Text(
+              "ID: ${data['id']}",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+            Text(
+              "PARENT-ID ${data['parent_id']}",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+            Text(
+              "EXTRAS: ${data['extra']}",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+          ],
         ),
       ),
     );
